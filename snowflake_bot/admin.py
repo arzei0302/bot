6f4e8c8fd74f40bcaf4fd2ef76bc02038ff3e171
arzei0302 <kabulov.arz@gmail.com>
2023-06-26 10:33:10 +0600

    qwert

---
+++ snowflake_bot/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from snowflake_bot.models import Project, UserAppeal
+
+admin.site.register(Project)
+admin.site.register(UserAppeal)
---
+++ snowflake_bot/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SnowflakeBotConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "snowflake_bot"
